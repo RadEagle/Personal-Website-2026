36b97433f542691e447806118463829d60bfa8c8
RadEagle <jonpaul2000@icloud.com>
2026-09-01 10:47:20 -0700

    initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ViteStarter, ViteNextSteps } from './Components/Vite'
+
+function App() {
+
+  return (
+    <>
+      <ViteStarter />
+      <ViteNextSteps />
+    </>
+  )
+}
+
+export default App
