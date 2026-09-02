c6fef80236d949b715a5c4571ad396387ee45126
RadEagle <jonpaul2000@icloud.com>
2026-09-02 14:30:12 -0700

    shorten menu transition durations

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -12,7 +12,7 @@ function HamburgerMenu(props: DropdownProps) {
       className={`text-3xl md:hidden focus:outline-none cursor-pointer relative w-8 h-8  ${props.active ? "toggle-btn" : ""}`}
       onClick={props.onClick}
     >
-      <div className="bg-white w-8 h-1 rounded absolute top-4 -mt-0.5 transition-all duration-500 before:content-[''] before:bg-white before:w-8 before:h-1 before:absolute before:-translate-x-4 before:-translate-y-3 before:transition-all before:duration-500 after:content-[''] after:bg-white after:w-8 after:h-1 after:absolute after:-translate-x-4 after:translate-y-3 after:transition-all after:duration-500"></div>
+      <div className="bg-white w-8 h-1 rounded absolute top-4 -mt-0.5 transition-all duration-300 before:content-[''] before:bg-white before:w-8 before:h-1 before:absolute before:-translate-x-4 before:-translate-y-3 before:transition-all before:duration-300 after:content-[''] after:bg-white after:w-8 after:h-1 after:absolute after:-translate-x-4 after:translate-y-3 after:transition-all after:duration-300"></div>
     </button>
   );
 }
