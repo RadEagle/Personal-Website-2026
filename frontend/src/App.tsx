240636b820dbcbbfe0bbbf65a8c5c04a26409d8b
RadEagle <jonpaul2000@icloud.com>
2026-09-02 14:21:32 -0700

    create base header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react'
 import './App.css'
+import { Header } from './Components/Header'
 import { ViteStarter, ViteNextSteps } from './Components/Vite'
 
 function App() {
+  // set the title of the page
+  useEffect(() => {
+    document.title = "Jon's Homepage"
+  }, [])
 
   return (
     <>
+      <Header />
       <ViteStarter />
       <ViteNextSteps />
     </>
