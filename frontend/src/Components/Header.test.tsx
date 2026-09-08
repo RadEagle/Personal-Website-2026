e2cffa8d2b1b52df4c95b6e496412c289bcba4dd
RadEagle <jonpaul2000@icloud.com>
2026-09-08 15:19:13 -0700

    finish writing tests for hamburger menu

--- frontend/src/Components/Header.test.tsx
+++ frontend/src/Components/Header.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { expect, test } from "vitest";
 import { Header } from "./Header";
 
@@ -11,9 +12,45 @@ test("renders the hamburger button", () => {
 });
 
 test("mobile menu is hidden on load", () => {
-    const mobileMenu = screen.getByRole("navigation", {name: /mobile/i}).parentElement as HTMLElement
+  const mobileMenu = screen.getByRole("navigation", { name: /mobile/i })
+    .parentElement as HTMLElement;
 
-    expect(
-        mobileMenu
-      ).toHaveClass("hidden");
+  expect(mobileMenu).toHaveClass("hidden");
+});
+
+test("mobile menu is visible on click", async () => {
+  const user = userEvent.setup();
+  const hamburger = screen.getByRole("button", { name: /open menu/i });
+  const mobileMenu = screen.getByRole("navigation", { name: /mobile/i })
+    .parentElement as HTMLElement;
+
+  await user.click(hamburger);
+
+  expect(mobileMenu).toHaveClass("flex");
+});
+
+test("hamburger click removes menu", async () => {
+  const user = userEvent.setup();
+  const hamburger = screen.getByRole("button", { name: /open menu/i });
+  const mobileMenu = screen.getByRole("navigation", { name: /mobile/i })
+    .parentElement as HTMLElement;
+
+  await user.click(hamburger);
+
+  expect(mobileMenu).toHaveClass("hidden");
+});
+
+test("clicking on mobile menu removes menu", async () => {
+  const user = userEvent.setup();
+  const hamburger = screen.getByRole("button", { name: /open menu/i });
+  const mobileMenu = screen.getByRole("navigation", { name: /mobile/i })
+    .parentElement as HTMLElement;
+
+  await user.click(hamburger);
+
+  expect(mobileMenu).toHaveClass("flex");
+
+  await user.click(mobileMenu);
+
+  expect(mobileMenu).toHaveClass("hidden");
 });
