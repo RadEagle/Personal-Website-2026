462819384bfd894096f2b8ce371b6ecec2af5452
RadEagle <jonpaul2000@icloud.com>
2026-09-15 16:25:27 -0700

    adjust blob style to be more modern

--- frontend/src/Library/styles.ts
+++ frontend/src/Library/styles.ts
@@ -1,3 +1,3 @@
 // frontend/src/Library/styles.ts
 
-export const blob = "bg-indigo-950 rounded-xl p-5 grid gap-3";
+export const blob = "bg-indigo-950/60 border-2 border-indigo-900 rounded-xl p-5 grid gap-3";
