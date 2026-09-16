0e945a610d384d5205313289ebcea0328fe06ab6
RadEagle <jonpaul2000@icloud.com>
2026-09-16 10:49:52 -0700

    transplant kanji project from old website

--- frontend/src/Library/styles.ts
+++ frontend/src/Library/styles.ts
@@ -1,3 +1,4 @@
 // frontend/src/Library/styles.ts
 
-export const blob = "bg-indigo-950/60 border-2 border-indigo-900 rounded-xl p-5 grid gap-3";
+export const blob =
+  "bg-indigo-950/60 border-2 border-indigo-900 rounded-xl p-5 grid gap-3";
